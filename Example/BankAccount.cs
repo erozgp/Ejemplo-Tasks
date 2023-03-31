1c2d8db5e460a1db73f47d6f29b912e2806510d1
Erick <erozgp@gmail.com>
2023-03-31 12:56:39 -0600

    Agregar archivos de proyecto.

---
+++ Example/BankAccount.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    public class BankAccount
+    {
+        public object locked = new object();
+        public int Balance { get; set; }
+        public double Growth { get; set; }
+        
+
+        public async Task Deposit(int Money)
+        {
+            await Task.Run(() =>
+            {
+                Balance += Money;
+            });                       
+        }
+
+        //Con Locked y esperando que en donde se consuma se use Task.Add(Task.Factory.StartNew(() => { EL BLOQUE DE CÓDIGO QUE SE DESEA EJECUTAR })); para realizar una tarea de manera directa sin implementar algún metodo separado.
+        public void DepositLocked(int Money)
+        {
+            lock (locked)
+            {
+                Balance += Money;
+            }
+        }
+
+        public async Task Draw(int Money)
+        {
+            await Task.Run(() =>
+            {
+                Balance -= Money;
+            });
+        }
+        
+        //Con Locked y esperando que en donde se consuma se use Task.Add(Task.Factory.StartNew(() => { EL BLOQUE DE CÓDIGO QUE SE DESEA EJECUTAR })); para realizar una tarea de manera directa sin implementar algún metodo separado.
+        public void DrawLocked(int Money)
+        {
+            lock (locked)
+            {
+                Balance -= Money;
+            }
+        }
+
+        public async Task CalculateGrowth(double Percentage)
+        {
+            await Task.Run(() =>
+            {
+                Growth = (Balance * Percentage);
+            });            
+        }
+
+        public async Task<double> CalculateUsingTGrowth(double Percentage)
+        {
+            return await Task.Run(() =>
+            {
+                return (Balance * Percentage);
+            });
+        }
+
+        //Usando TaskCompletionSource para completar una tarea asíncrona manualmente
+        public Task<double> CalculateManuallyGrowth(double Percentage)
+        {
+            var taskCompletion = new TaskCompletionSource<double>();
+
+            Task.Run(() => 
+            { 
+                double Growth = (Balance * Percentage);
+                taskCompletion.SetResult(Growth);
+            });
+            return taskCompletion.Task;
+        }
+
+        //Usando TaskFactory como otra forma de crear tareas asíncronas y usando Task<T>
+        public async Task<double> CalculateFactoryGrowth(double Percentage)
+        {
+            return await Task.Factory.StartNew(() =>
+            {
+                return Balance * Percentage;
+            });
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,67 @@
+﻿using Example;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var bank = new BankAccount();
+            List<Task> tasks = new List<Task>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 1000; j++)
+                {
+                    //await bank.Deposit(100);
+
+                    tasks.Add(Task.Factory.StartNew(() =>
+                    {
+                        bank.DepositLocked(100);
+                    }));
+                    
+                }
+                for(int k = 0; k < 1000; k++)
+                {
+                    //await bank.Draw(50);
+                    tasks.Add(Task.Factory.StartNew(() =>
+                    {
+                        bank.DrawLocked(50);
+                    }));
+                }
+            }
+
+            //Normal para el rendimiento
+            await bank.CalculateGrowth(.10);
+
+            //Aplicando Task<T> para el rendimiento
+            double growthT = await bank.CalculateUsingTGrowth(.10);
+
+            //Aplicando TaskCompletionSource para el rendimiento
+            Task<double> growthTask = bank.CalculateManuallyGrowth(.10);
+            double growthManually = await growthTask;
+
+            //Aplicando TaskFactory para el rendimiento
+            double growthFactory = await bank.CalculateFactoryGrowth(.10);                       
+
+            Console.WriteLine($"Final balance is {bank.Balance}");
+
+            Console.WriteLine($"Final growth is {bank.Growth}");
+            Console.WriteLine($"Final growthT is {growthT}");
+            Console.WriteLine($"Final growthManually is {growthManually}");
+            Console.WriteLine($"Final growthFactory is {growthFactory}");
+
+
+
+            Console.ReadLine();
+        }
+
+        //Investigar cómo usar las tasks, 4 formas aprox
+        //Repasar lo de locked
+    }
+}
